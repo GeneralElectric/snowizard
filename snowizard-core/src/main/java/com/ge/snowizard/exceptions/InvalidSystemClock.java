94b8e5e9bb0bff110ec5f913227e69f1c894f426
Justin Plock <justin.plock@ge.com>
2013-09-23 12:51:31 -0400

    Fixed invalid use of the {@inheritDoc} tag issues

--- snowizard-core/src/main/java/com/ge/snowizard/exceptions/InvalidSystemClock.java
+++ snowizard-core/src/main/java/com/ge/snowizard/exceptions/InvalidSystemClock.java
@@ -7,22 +7,18 @@ public class InvalidSystemClock extends Exception {
      */
     private static final long serialVersionUID = 1L;
 
-    /** {@inheritDoc} */
     public InvalidSystemClock() {
         super();
     }
 
-    /** {@inheritDoc} */
     public InvalidSystemClock(final Throwable cause) {
         super(cause);
     }
 
-    /** {@inheritDoc} */
     public InvalidSystemClock(final String message) {
         super(message);
     }
 
-    /** {@inheritDoc} */
     public InvalidSystemClock(final String message, final Throwable cause) {
         super(message, cause);
     }
