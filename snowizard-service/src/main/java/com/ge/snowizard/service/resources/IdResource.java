26795824d6d96c8e29a920ae681896054ef0fdf1
Tim Bart <tim@sayhello.com>
2013-11-12 17:06:30 -0800

    Adding batch endpoint to IdResource

--- snowizard-service/src/main/java/com/ge/snowizard/service/resources/IdResource.java
+++ snowizard-service/src/main/java/com/ge/snowizard/service/resources/IdResource.java
@@ -1,15 +1,22 @@
 package com.ge.snowizard.service.resources;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.WebApplicationException;
+
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response.Status;
+
+import com.ge.snowizard.api.protos.SnowizardProtos.SnowizardBatchResponse;
+import com.ge.snowizard.api.protos.SnowizardProtos.SnowizardRequest;
+import com.google.protobuf.InvalidProtocolBufferException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.ge.snowizard.api.Id;
@@ -22,6 +29,8 @@ import com.ge.snowizard.service.core.MediaTypeAdditional;
 import com.sun.jersey.api.json.JSONWithPadding;
 import com.yammer.dropwizard.jersey.caching.CacheControl;
 import com.yammer.metrics.annotation.Timed;
+
+import java.util.ArrayList;
 
 @Path("/")
 public class IdResource {
@@ -59,6 +68,15 @@ public class IdResource {
             throw new WebApplicationException(SnowizardError.newResponse(
                     Status.INTERNAL_SERVER_ERROR, e.getMessage()));
         }
+    }
+
+    public ArrayList<Long> getIds(int n, final String agent) {
+
+        ArrayList<Long> ids = new ArrayList<Long>();
+        for(int i=0; i < n; i++) {
+            ids.add(getId(agent));
+        }
+        return ids;
     }
 
     /**
@@ -130,5 +148,32 @@ public class IdResource {
     public SnowizardResponse getIdAsProtobuf(
             @HeaderParam("User-Agent") final String agent) {
         return SnowizardResponse.newBuilder().setId(getId(agent)).build();
+    }
+
+    /**
+     * Get a new batch of IDs as a Google Protocol Buffer response
+     *
+     * @param agent
+     *            User Agent
+     * @responseMessage 400 Invalid User-Agent header
+     * @responseMessage 500 Invalid system clock
+     * @return batch of generated ID
+     */
+    @POST
+    @Timed
+    @Produces(MediaTypeAdditional.APPLICATION_PROTOBUF)
+    @CacheControl(mustRevalidate = true, noCache = true, noStore = true)
+    public SnowizardBatchResponse getBatchIdAsProtobuf(byte[] body,
+            @HeaderParam("User-Agent") final String agent) {
+        SnowizardRequest req;
+        try {
+            req = SnowizardRequest.parseFrom(body);
+        } catch (InvalidProtocolBufferException e) {
+            LOGGER.error("Failed decoding protobuf request");
+            throw new WebApplicationException(SnowizardError.newResponse(
+                    Status.INTERNAL_SERVER_ERROR, e.getMessage()));
+
+        }
+        return SnowizardBatchResponse.newBuilder().addAllIds(getIds(req.getBatchSize(), agent)).build();
     }
 }
